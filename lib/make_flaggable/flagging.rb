428e622dbb60e9ae908ecf46f50e653bff2176cf
afly <aflymail@gmail.com>
2013-01-15 20:23:30 +0800

    add attr_accessible in flagging.rb for rails 3.2+

--- lib/make_flaggable/flagging.rb
+++ lib/make_flaggable/flagging.rb
@@ -3,5 +3,9 @@ module MakeFlaggable
     belongs_to :flaggable, :polymorphic => true
     belongs_to :flagger, :polymorphic => true
     scope :with_flag, lambda { |flag| where(:flag => flag.to_s) }
+
+    attr_accessible :flaggable_type,:flaggable_id,:flagger_type,:flagger_id,
+      :flag,:flagger
   end
 end
+
